5e9d213d01894e363ae3b973899c1934af7596c5
souravdatta <soura.jagat@gmail.com>
2013-12-25 13:28:09 +0530

    Initial commit

---
+++ jobj.js
@@ -0,0 +1,42 @@
+Jobj = (function () {
+    var Jobj = {};
+    
+    // Extend a class to create a new one   
+    Jobj.extend = function (klass, objFlags) {
+        var dklass = function () {};
+        
+        dklass.prototype = new klass();
+        dklass.prototype.parent = klass.prototype;
+        if (objFlags) {
+            var prop;
+            for (prop in objFlags) {
+                if (({}).hasOwnProperty.call(objFlags, prop) &&
+                    prop != 'parent') {
+                    dklass.prototype[prop] = objFlags[prop];
+                }
+            }
+        }
+        
+        return dklass;
+    };
+    
+    Jobj.create = function(klass) {
+        var args = [], indx;
+        for (indx = 1; indx < arguments.length; indx++) {
+            args.push(arguments[indx]);
+        }
+        
+        var inst = new klass();
+        if (inst.init && typeof(inst.init) == 'function') {
+            inst.init.apply(inst, args);
+        }
+        
+        return inst;
+    };
+    
+    Jobj.Class = function () {
+        this.name = 'a Class';
+    };
+    
+    return Jobj;
+})();
